38f04794f45065ee44a986a6771775645ce55ac5
mutgarth <lucasmenesesbs@gmail.com>
2022-02-13 21:01:44 -0300

    Improved utils and populate historical price script

--- src/get_stock_data.py
+++ src/get_stock_data.py
@@ -1,22 +1,34 @@
+from utils import get_hist_data, get_index_positions
+import csv 
+import os
 
-import pandas_datareader.data as pdr
-import yfinance as yf
-from datetime import date
-import csv
+parent_dir = os.path.dirname(os.getcwd())
+data_dir = data_dir = os.path.join(os.path.join(parent_dir,"data"), "b3symbols.csv")
 
-
-def get_data(symbols, d0, d1, period):
-    yf.pdr_override()
-    stocks = pdr.get_data_yahoo(symbols, d0, d1, interval=period, as_panel=False)
-    return stocks
-
-with open('b3symbols.csv', newline='') as f:
+with open(data_dir, newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
 
 symbols = [''.join(x) for x in data]
+index_position = get_index_positions(symbols, ' Código ')
+#get only stocks, excluding FII and BDRS 
+symbols = symbols[index_position[0]+1:index_position[1]]
 
-d0 = date(2021, 4, 20)
-d1 = date.today()
+symbols = [symbols[k].strip() +'.SA' for k in range(len(symbols))]
+indices = ['^BVSP', '^IEE', 'IMAT.SA']
 
-stocks = get_data(symbols,d0, d1, '1d')
+hist_data = get_hist_data(symbols+indices, '1d')
+
+adj_close = hist_data['Adj Close']
+close_price = hist_data['Close']
+high_price = hist_data['High']
+open_price = hist_data['Open']
+low_price = hist_data['Low']
+volume = hist_data['Volume']
+
+adj_close.to_csv(os.path.join(os.path.join(parent_dir,"data"), "df_adj_close.csv"))
+close_price.to_csv(os.path.join(os.path.join(parent_dir,"data"), "df_close.csv"))
+open_price.to_csv(os.path.join(os.path.join(parent_dir,"data"), "df_open.csv"))
+high_price.to_csv(os.path.join(os.path.join(parent_dir,"data"), "df_high.csv"))
+low_price.to_csv(os.path.join(os.path.join(parent_dir,"data"), "df_low.csv"))
+volume.to_csv(os.path.join(os.path.join(parent_dir,"data"), "volume.csv"))
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas_datareader.data as pdr
+import yfinance as yf
 
-def get_web_content(url):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+def get_webdriver_content(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options = chrome_options, service=Service(ChromeDriverManager().install()))
     driver.get(url)
     content = driver.page_source
     return content
 
+def request_web_content(url):
+    page = requests.get(url)
+    return page.text, page.status_code
 
+def get_hist_data(symbols, interval):
+    yf.pdr_override()
+    stocks = pdr.get_data_yahoo(symbols, period='max', interval=interval, group_by = 'column')
+    return stocks
 
+def get_index_positions(list_of_elems, element):
+    index_pos_list = []
+    index_pos = 0
+    while True:
+        try:
+            index_pos = list_of_elems.index(element, index_pos)
+            index_pos_list.append(index_pos)
+            index_pos += 1
+        except ValueError as e:
+            break
+    return index_pos_list
